d57f26bf5d53db0e80b829c2f66bf508f7f317a9
K1LLER-R0N4N <138888767+K1LLER-R0N4N@users.noreply.github.com>
2023-07-20 19:35:27 +0200

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,26 @@
+Groccery_List = ["apple ", "banana ", "carrot ", "cucumber ", "tomato ", "pepper ", "pear ", "tomato ", "pepper ", "pear " ]
+Groccery_List[2] = "potato"
+Groccery_List[1:4] = ["potato ", "watermelon"]
+Groccery_List.insert(2, "milk")
+Groccery_List.append("apple")
+#Groccery_List.remove("pear")
+Groccery_List.pop(3)
+Groccery_List.pop()
+#del Groccery_List[6]
+#del Groccery_List
+Groccery_List.clear()
+
+
+print(Groccery_List)
+
+print(len(Groccery_List))
+
+#print(Groccery_List[0])
+
+print(Groccery_List[1:4])
+
+#print(Groccery_List[-1])
+
+print(Groccery_List[0:])
+
+print(Groccery_List)
