c7ebdcc94295b5b7c9f2e0a8b5591e862cc4f0ef
K1LLER-R0N4N <138888767+K1LLER-R0N4N@users.noreply.github.com>
2023-07-24 11:30:58 +0200

    Add files via upload

---
+++ methods.py
@@ -0,0 +1,33 @@
+Shooping_List = ["apple ", "banana ", "carrot ", "cucumber ", "tomato ", "pepper ", "pear ", "tomato ", "pepper ", "pear " ]
+Fruit_List = ["cucumber ", "tomato ", "pepper ",]
+All_list = Shooping_List + Fruit_List
+Shooping_List[2] = "potato "
+Shooping_List[1:4] = ["potato ", "watermelon"]
+Shooping_List.insert(2, "milk")
+Shooping_List.append("apple")
+Shooping_List.remove("pear ")
+Shooping_List.pop(3)
+Shooping_List.pop()
+del Shooping_List[6]
+#del Shooping_list()
+Shooping_List.clear()
+Shooping_List.sort()
+All_list.sort()
+Shooping_List.reverse()
+All_list.reverse()
+
+print(Shooping_List)
+
+print(len(Shooping_List))
+
+print(All_list[6])
+
+print(Shooping_List[1:4])
+
+print(All_list[-1])
+
+print(Shooping_List[0:])
+
+print(Shooping_List)
+
+print(All_list)
